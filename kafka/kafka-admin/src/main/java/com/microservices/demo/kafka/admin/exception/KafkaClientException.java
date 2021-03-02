991891a285ef9fc302816418c6bd51b36f479582
Weibin SHen <weibinizer@gmail.com>
2021-03-01 20:53:42 -0800

    Kafka admin module createTopics(), checkTopicsCreated() and checkSchemaRegistry()

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.kafka.admin.exception;
+
+// Exception class for kafka client error situations.
+public class KafkaClientException extends RuntimeException {
+
+    public KafkaClientException() {
+    }
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
