8d609ef966ec7611405871d384774a078dd956d0
Weibin SHen <weibinizer@gmail.com>
2021-03-20 21:48:53 -0700

    Selected JCE for security, as Jasypt doesn't use PBKDF2

---
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // There are encrypt and decrypt endpoint already compatible with Spring boot CLI.
+    // Here the ignoring will bypass these two end points from authentication of spring security
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/encrypt/**")
+                .antMatchers("/decrypt/**");
+        super.configure(web);
+    }
+}
