f22f7bfd19319b2177b4475fe29d8e23a1e11033
Weibin SHen <weibinizer@gmail.com>
2021-03-21 18:48:26 -0700

    getting ready to build config-server docker image. Created shell script to health-check the config-server

--- config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .antMatchers("/actuator/**")
                 .antMatchers("/encrypt/**")
                 .antMatchers("/decrypt/**");
         super.configure(web);
---
+++ docker-compose/check-config-server-started.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# check-config-server-started.sh
+
+# depends_on from Docker is not enough for us,
+# because that only checks if the Docker container is up,
+# not whether our application inside the container is up.
+
+apt-get update -y
+
+yes | apt-get install curl
+
+curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+
+echo "result status code:" "$curlResult"
+
+while [[ ! $curlResult == "200" ]]; do
+  >&2 echo "Config server is not up yet!"
+  sleep 2
+  curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+done
+
+# move on with launching our service. This is the original entrypoint.
+./cnb/lifecycle/launcher
