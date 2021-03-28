5c0c5ef10176899e4d6e173ba7659fe90b7b2d53
Weibin SHen <weibinizer@gmail.com>
2021-03-27 20:00:02 -0700

    elastic-index-client module

--- elastic/elastic-model/src/main/java/com/microservices/demo/elastic/model/index/IndexModel.java
+++ elastic/elastic-model/src/main/java/com/microservices/demo/elastic/model/index/IndexModel.java
@@ -1,5 +1,6 @@
 package com.microservices.demo.elastic.model.index;
 
 public interface IndexModel {
+    // we declare this method because we need to use it with the document template in ElasticIndexUtil.
     String getId();
 }
