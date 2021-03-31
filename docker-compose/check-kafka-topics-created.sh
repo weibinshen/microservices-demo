73b042443efeb676a9eeea364a84cd32e1ab4554
Weibin SHen <weibinizer@gmail.com>
2021-03-30 21:17:19 -0700

    a few more tips for running and validating kafka-to-elastic service

--- docker-compose/check-kafka-topics-created.sh
+++ docker-compose/check-kafka-topics-created.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # check-kafka-topics-created.sh
 
+# We need to make sure we run 'chmod +x check-kafka-topics-created.sh'
+# After the service is running, its console should log the doc ID.
+# then we can query it from Postman: e.g. localhost:9200/twitter-index/_search?q=id:1376017922397630464
+
 apt-get update -y
 
 yes | apt-get install kafkacat
